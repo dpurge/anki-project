3437f1150e5658109b8ca613967eeeabe322533e
Janusz Prusaczyk (JJPR) <jjpr@demant.com>
2020-05-20 17:12:26 +0200

    Make template variables start with uppercase

--- src/ankiproject/get_anki_note.py
+++ src/ankiproject/get_anki_note.py
@@ -1,9 +1,5 @@
 from genanki import Note, guid_for
 
-#class AnkiNote(Note):
-#  @property
-#  def guid(self):
-#    return genanki.guid_for(self.fields[0], self.fields[1], self.fields[2])
 
 def get_anki_note(item, model, template):
     fmt = item.meta['format']
@@ -11,11 +7,11 @@ def get_anki_note(item, model, template):
         tags = template['Tags']['template'].render(**item.data).split(' ')
         _id = template['ID']['template'].render(**item.data)
     elif  fmt == 'data/text':
-        tags = template['Tags']['template'].render(text = item.data).split(' ')
-        _id = template['ID']['template'].render(text = item.data)
+        tags = template['Tags']['template'].render(Text = item.data).split(' ')
+        _id = template['ID']['template'].render(Text = item.data)
     elif fmt == 'data/list':
-        tags = template['Tags']['template'].render(items = item.data).split(' ')
-        _id = template['ID']['template'].render(items = item.data)
+        tags = template['Tags']['template'].render(Items = item.data).split(' ')
+        _id = template['ID']['template'].render(Items = item.data)
     else:
         raise Exception('Unsupported item format: {fmt}'.format(fmt = fmt))
 
@@ -33,6 +29,7 @@ def get_anki_note(item, model, template):
         
         fields.append(contents)
 
+    #print(fields)
     return Note(
         guid = guid_for(_id),
         model = model,
